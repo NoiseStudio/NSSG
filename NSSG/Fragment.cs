f1d6849cdb993a75d115d90947f597773330f668
Vixen <infvix@gmail.com>
2023-09-12 18:20:46 +0200

    Add simple static site generator

---
+++ NSSG/Fragment.cs
@@ -0,0 +1,76 @@
+﻿using System.Xml;
+
+namespace NSSG;
+
+internal class Fragment {
+
+    public string Name { get; }
+    public XmlDocument Document { get; }
+
+    public Fragment(string name, XmlDocument document) {
+        Name = name;
+        Document = document;
+    }
+    public void ReplaceNode(XmlNode nodeToReplace) {
+        XmlNode newNode = nodeToReplace.OwnerDocument!.ImportNode(Document.DocumentElement!, true);
+        ReplaceContinue(newNode, nodeToReplace);
+
+        XmlAttributeCollection? attributes = nodeToReplace.Attributes;
+
+        nodeToReplace.ParentNode!.ReplaceChild(newNode, nodeToReplace);
+        if (attributes is null)
+            return;
+
+        ReplaceAttributesInner(newNode, attributes);
+        ReplaceAttributes(newNode, attributes);
+    }
+
+    private void ReplaceContinue(XmlNode newNode, XmlNode nodeToReplace) {
+        foreach (XmlNode node in newNode.ChildNodes) {
+            if (node.Name != "Continue") {
+                ReplaceContinue(node, nodeToReplace);
+                continue;
+            }
+
+            XmlNode last = node;
+            foreach (XmlNode child in nodeToReplace.ChildNodes) {
+                XmlNode n = child.CloneNode(true);
+                newNode.InsertAfter(n, last);
+                last = n;
+            }
+            newNode.RemoveChild(node);
+            break;
+        }
+    }
+
+    private void ReplaceAttributes(XmlNode newNode, XmlAttributeCollection attributes) {
+        foreach (XmlNode node in newNode.ChildNodes) {
+            ReplaceAttributesInner(node, attributes);
+            ReplaceAttributes(node, attributes);
+        }
+    }
+
+    private void ReplaceAttributesInner(XmlNode newNode, XmlAttributeCollection attributes) {
+        if (newNode.ChildNodes.Count != 0)
+            return;
+
+        if (newNode.InnerText.StartsWith("${") && newNode.InnerText.EndsWith("}")) {
+            string name = newNode.InnerText[2..^1];
+            if (attributes[name] is not null)
+                newNode.InnerText = attributes[name]!.Value;
+        }
+
+        XmlAttributeCollection? a = newNode.Attributes;
+        if (a is null)
+            return;
+
+        foreach (XmlAttribute attribute in a) {
+            if (attribute.Value.StartsWith("${") && attribute.Value.EndsWith("}")) {
+                string name = attribute.Value[2..^1];
+                if (attributes[name] is not null)
+                    attribute.Value = attributes[name]!.Value;
+            }
+        }
+    }
+
+}
---
+++ NSSG/Program.cs
@@ -0,0 +1,54 @@
+﻿using NSSG;
+using System.Collections.Concurrent;
+using System.Xml;
+
+string path = Directory.GetCurrentDirectory();
+int pathCount = path.EndsWith('/') || path.EndsWith('\\') ? path.Length : path.Length + 1;
+
+// Load fragments.
+const string fragmentsPath = "fragments";
+ConcurrentBag<Fragment> fragments = new ConcurrentBag<Fragment>();
+
+string fullFragmentsPath = Path.Combine(path, fragmentsPath);
+if (Directory.Exists(fullFragmentsPath)) {
+    Parallel.ForEach(Directory.EnumerateFiles(fullFragmentsPath, "*.html", SearchOption.AllDirectories), x => {
+        XmlDocument document = new XmlDocument();
+        document.Load(x);
+
+        int i = pathCount + fragmentsPath.Length + 1;
+        string name = x.Substring(pathCount + fragmentsPath.Length + 1, x.LastIndexOf('.') - i)
+            .Replace('\\', '/');
+        fragments.Add(new Fragment(name, document));
+    });
+}
+
+// Compute pages.
+const string pagesPath = "pages";
+
+string fullPagesPath = Path.Combine(path, pagesPath);
+if (!Directory.Exists(fullPagesPath))
+    return;
+
+Directory.CreateDirectory("output");
+    
+Parallel.ForEach(Directory.EnumerateFiles(fullPagesPath, "*.html", SearchOption.AllDirectories), x => {
+    XmlDocument document = new XmlDocument();
+    document.Load(x);
+
+    foreach (Fragment fragment in fragments) {
+        XmlNodeList? nodeList = document.GetElementsByTagName(fragment.Name);
+        if (nodeList is null)
+            continue;
+
+        for (int i = 0; i < nodeList.Count; i++) {
+            XmlNode node = nodeList[i]!;
+            fragment.ReplaceNode(node);
+        }
+    }
+
+    foreach (XmlNode node in document.SelectNodes("//comment()")!)
+        node.ParentNode!.RemoveChild(node);
+
+    string name = x.Substring(pathCount + pagesPath.Length + 1);
+    File.WriteAllText(Path.Combine(path, "output", name), document.OuterXml);
+});
